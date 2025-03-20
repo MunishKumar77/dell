8409d19ba09e43293dec45ef65aa9af6c0cc19dd
Munish <cu.16bet1033@gmail.com>
2025-03-20 10:23:23 +0530

    a to link

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,11 +1,12 @@
 import "./home.css";
+import { Link } from "react-router-dom";
 
 const HomePage = () => {
   return (
     <div className="home-container">
       <h1>Welcome to Our Website</h1>
-      <a href="/login" className="btn">Login</a>
-      <a href="/signup" className="btn">Sign Up</a>
+      <Link href="/login" className="btn">Login</Link>
+      <Link href="/signup" className="btn">Sign Up</Link>
     </div>
   );
 };
